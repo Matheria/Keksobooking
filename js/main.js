d39eb4eed75e2baf6034f9ee4450172b29596307
Matheria <sensenevercame@gmail.com>
2021-07-03 01:29:16 +0500

    выполняет Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {createAdv} from './mocks/adv.js';
 import {createAdCard} from './ad-card.js';
+import {inactivePage} from './forms-state.js';
+import {activePage} from './forms-state.js';
 
 const ad = createAdv();
 const adCard = createAdCard(ad);
@@ -7,3 +9,5 @@ const adCard = createAdCard(ad);
 const mapCanvas = document.querySelector('.map__canvas');
 
 mapCanvas.appendChild(adCard);
+
+inactivePage();
--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,17 @@
-export function getRandomNumberInRange(min, max) {
+export const getRandomNumberInRange = (min, max) => {
   if (min >= max || min < 0) {
     throw new Error('Допускаются только положительные числа в диапазоне от меньшего к большему и не равные друг другу');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-export function getRandomFloatNumberInRange(min, max, accuracy) {
+export const getRandomFloatNumberInRange = (min, max, accuracy) => {
   if (min >= max || min < 0) {
     throw new Error('Допускаются только положительные числа в диапазоне от меньшего к большему и не равные друг другу');
   }
   const randomFloatNumber = min + Math.random() * (max - min);
   return parseFloat(randomFloatNumber.toFixed(accuracy));
-}
+};
 
 export const getRandomArrayElement = (elements) => elements[getRandomNumberInRange(0, elements.length - 1)];
 
