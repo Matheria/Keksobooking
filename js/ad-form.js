b4b136f8662743684b6bf9b5b94ea620b3d00380
Matheria <sensenevercame@gmail.com>
2021-07-07 15:49:47 +0500

    fixup! fixup! Выполняет Правда или действие (часть 1)

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,4 +1,5 @@
 import {TypeOfHousing} from './enums.js';
+import {pluralize} from './utils.js';
 
 const adForm = document.querySelector('.ad-form');
 
@@ -6,8 +7,6 @@ if (adForm === null) {
   throw new Error ('adForm не найден');
 }
 
-const minTitleLength = 30;
-const maxTitleLength = 100;
 const minPriceOfHousingType = {
   [TypeOfHousing.bungalow]: 0,
   [TypeOfHousing.flat]: 1000,
@@ -44,12 +43,14 @@ if (
 }
 
 export const handleTitleInputChange = () => {
-  const adTitle = titleInput.value.length;
+  if (titleInput.value.length < titleInput.minLength) {
+    const requredSymbolsCount = titleInput.minLength - titleInput.value.length;
 
-  if (adTitle < minTitleLength) {
-    titleInput.setCustomValidity(`Ещё ${  minTitleLength - adTitle } символов.`);
-  } else if (adTitle > maxTitleLength) {
-    titleInput.setCustomValidity(`Удалите лишние ${  adTitle - maxTitleLength } символов.`);
+    titleInput.setCustomValidity(`Ещё ${requredSymbolsCount} ${pluralize(requredSymbolsCount, ['символ', 'символа', 'символов'])}.`);
+  } else if (titleInput.value.length > titleInput.maxLength) {
+    const symbolsToDeleteCount = titleInput.value.length - titleInput.maxLength;
+
+    titleInput.setCustomValidity(`Удалите лишние ${symbolsToDeleteCount} ${pluralize(symbolsToDeleteCount, ['символ', 'символа', 'символов'])}.`);
   } else {
     titleInput.setCustomValidity('');
   }
@@ -62,34 +63,35 @@ export const handleTypeSelectChange = () => {
 };
 
 export const handlePriceInputChange = () => {
-  const pricePerNight = priceInput.value;
-  const typeOfHousing = typeSelect.value;
-  const minPrice = minPriceOfHousingType[typeOfHousing];
-  const maxPrice = 1000000;
+  const price = Number(priceInput.value);
 
-  if (pricePerNight < minPrice) {
-    priceInput.setCustomValidity(`Стоимость должна быть не ниже ${minPrice}`);
-  } else if (pricePerNight > maxPrice) {
-    priceInput.setCustomValidity(`Стоимость не должна превышать ${maxPrice}`);
+  if (price < Number(priceInput.min)) {
+    priceInput.setCustomValidity(`Стоимость должна быть не ниже ${priceInput.min}`);
+  } else if (price > Number(priceInput.max)) {
+    priceInput.setCustomValidity(`Стоимость не должна превышать ${priceInput.max}`);
   } else {
     priceInput.setCustomValidity('');
   }
   priceInput.reportValidity();
 };
 
-export const handleRoomSelectChange = () => {
+const updateCapacitySelect = () => {
   for (const option of capacitySelect.options) {
-    option.disabled = !roomCapacityByRooms[roomSelect.value].includes(option.value);
+    option.hidden = !roomCapacityByRooms[roomSelect.value].includes(option.value);
   }
-  capacitySelect.value = roomCapacityByRooms[roomSelect.value].includes(capacitySelect.value) ? capacitySelect.value : roomCapacityByRooms[roomSelect.value][0];
+  capacitySelect.value = roomCapacityByRooms[roomSelect.value].includes(capacitySelect.value) ? capacitySelect.value : '';
+};
+
+export const handleRoomSelectChange = () => {
+  updateCapacitySelect();
 };
 
 export const handleTimeInSelectChange = () => {
-  timeInSelect.value = timeOutSelect.value;
+  timeOutSelect.value = timeInSelect.value;
 };
 
 export const handleTimeOutSelectChange = () => {
-  timeOutSelect.value = timeInSelect.value;
+  timeInSelect.value = timeOutSelect.value;
 };
 
 export const deactivateAdForm = () => {
@@ -112,5 +114,7 @@ titleInput.addEventListener('input', handleTitleInputChange);
 typeSelect.addEventListener('change', handleTypeSelectChange);
 priceInput.addEventListener('input', handlePriceInputChange);
 roomSelect.addEventListener('change', handleRoomSelectChange);
-timeInSelect.addEventListener('change', handleTimeOutSelectChange);
-timeOutSelect.addEventListener('change', handleTimeInSelectChange);
+timeInSelect.addEventListener('change', handleTimeInSelectChange);
+timeOutSelect.addEventListener('change', handleTimeOutSelectChange);
+
+updateCapacitySelect();
